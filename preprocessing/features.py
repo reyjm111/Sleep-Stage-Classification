28695f7030a143c1d848e1631b8264ac510fabbf
reyjm111 <155209812+reyjm111@users.noreply.github.com>
2026-04-17 19:02:52 -0700

    Added spectral features

--- preprocessing/features.py
+++ preprocessing/features.py
@@ -1,6 +1,7 @@
 from scipy.signal import welch
 import numpy as np
 import pandas as pd
+from specparam import SpectralModel
 
 def feature_extraction(epochs):
 
@@ -9,16 +10,17 @@ def feature_extraction(epochs):
         'theta': (4, 8),
         'alpha': (8, 13),
         'beta': (13, 30),
-        'gamma': (30, 100),
+        'gamma': (30, 45),
     }
 
     fs = 200
     epoch_len = 30
-    nperseg = fs * epoch_len
+    nperseg = 1024
+    noverlap=512
     eps = 1e-12 # to prevent division by 0
 
     max_nan_frac_per_ch=0.05
-    min_channels=4
+    min_channels=1
     features = []
 
     # iterate throug each epoch and extract features
@@ -31,25 +33,62 @@ def feature_extraction(epochs):
         valid_epoch = epoch[keep_mask]
         if valid_epoch.shape[0] < min_channels:
             features.append({
+                # bandpower
                 'delta_abs_bandpower': np.nan,
                 'theta_abs_bandpower': np.nan,
                 'alpha_abs_bandpower': np.nan,
                 'beta_abs_bandpower': np.nan,
                 'gamma_abs_bandpower': np.nan,
+
+                'delta_std_bandpower': np.nan,
+                'theta_std_bandpower': np.nan,
+                'alpha_std_bandpower': np.nan,
+                'beta_std_bandpower': np.nan,
+                'gamma_std_bandpower': np.nan,
+
+                # LR diff
+                'delta_lr_diff': np.nan,
+                'theta_lr_diff': np.nan,
+                'alpha_lr_diff': np.nan,
+                'beta_lr_diff': np.nan,
+                'gamma_lr_diff': np.nan,
+
+                # relative
                 'delta_rel_bandpower': np.nan,
                 'theta_rel_bandpower': np.nan,
                 'alpha_rel_bandpower': np.nan,
                 'beta_rel_bandpower': np.nan,
                 'gamma_rel_bandpower': np.nan,
+
+                # ratios
                 'delta_theta_ratio': np.nan,
                 'delta_alpha_ratio': np.nan,
                 'theta_alpha_ratio': np.nan,
                 'delta_beta_ratio': np.nan,
                 'alpha_beta_ratio': np.nan,
+                'theta_beta_ratio': np.nan,
+                'alpha_delta_ratio': np.nan,
+                'theta_delta_ratio': np.nan,
+
+                # peak
+                'peak_frequency': np.nan,
+
+                # Hjorth
                 'hjorth_activity': np.nan,
                 'hjorth_mobility': np.nan,
                 'hjorth_complexity': np.nan,
+
+                # entropy
                 'spectral_entropy': np.nan,
+
+                # specparam
+                'aperiodic_exponent_mean': np.nan,
+                'aperiodic_exponent_std': np.nan,
+                'aperiodic_offset_mean': np.nan,
+                'aperiodic_offset_std': np.nan,
+                'alpha_peak_power_mean': np.nan,
+                'alpha_peak_freq_mean': np.nan,
+                'aperiodic_exponent_lr_diff': np.nan,
             })
             continue
         
@@ -77,25 +116,62 @@ def feature_extraction(epochs):
         # final safeguard
         if np.isnan(valid_epoch).any():
             features.append({
+                # bandpower
                 'delta_abs_bandpower': np.nan,
                 'theta_abs_bandpower': np.nan,
                 'alpha_abs_bandpower': np.nan,
                 'beta_abs_bandpower': np.nan,
                 'gamma_abs_bandpower': np.nan,
+
+                'delta_std_bandpower': np.nan,
+                'theta_std_bandpower': np.nan,
+                'alpha_std_bandpower': np.nan,
+                'beta_std_bandpower': np.nan,
+                'gamma_std_bandpower': np.nan,
+
+                # LR diff
+                'delta_lr_diff': np.nan,
+                'theta_lr_diff': np.nan,
+                'alpha_lr_diff': np.nan,
+                'beta_lr_diff': np.nan,
+                'gamma_lr_diff': np.nan,
+
+                # relative
                 'delta_rel_bandpower': np.nan,
                 'theta_rel_bandpower': np.nan,
                 'alpha_rel_bandpower': np.nan,
                 'beta_rel_bandpower': np.nan,
                 'gamma_rel_bandpower': np.nan,
+
+                # ratios
                 'delta_theta_ratio': np.nan,
                 'delta_alpha_ratio': np.nan,
                 'theta_alpha_ratio': np.nan,
                 'delta_beta_ratio': np.nan,
                 'alpha_beta_ratio': np.nan,
+                'theta_beta_ratio': np.nan,
+                'alpha_delta_ratio': np.nan,
+                'theta_delta_ratio': np.nan,
+
+                # peak
+                'peak_frequency': np.nan,
+
+                # Hjorth
                 'hjorth_activity': np.nan,
                 'hjorth_mobility': np.nan,
                 'hjorth_complexity': np.nan,
+
+                # entropy
                 'spectral_entropy': np.nan,
+
+                # specparam
+                'aperiodic_exponent_mean': np.nan,
+                'aperiodic_exponent_std': np.nan,
+                'aperiodic_offset_mean': np.nan,
+                'aperiodic_offset_std': np.nan,
+                'alpha_peak_power_mean': np.nan,
+                'alpha_peak_freq_mean': np.nan,
+                'aperiodic_exponent_lr_diff': np.nan,
             })
             continue
         
@@ -104,14 +180,20 @@ def feature_extraction(epochs):
             valid_epoch,
             fs=fs,
             nperseg=nperseg, 
+            noverlap=noverlap, 
             axis=1
         )
-
+        
         abs_power_features = {}
         rel_power_features = {}
         ratio_features = {}
         hjorth_features = {}
         spectral_entropy_features = {}
+        peak_features = {}
+
+        # Peak Freq
+        peak_freqs = f[np.argmax(Pxx, axis=1)]
+        peak_features["peak_frequency"] = np.mean(peak_freqs)
 
         # absolute bandpower
         for band, (f_low, f_high) in bands.items():
@@ -119,9 +201,17 @@ def feature_extraction(epochs):
             band_psd = Pxx[:, band_mask]
             bandpower_per_ch = np.trapezoid(band_psd, f[band_mask], axis=1) # area under the curve per ch
             abs_power_features[f'{band}_abs_bandpower'] = np.nanmean(bandpower_per_ch) # average across channels
+            abs_power_features[f'{band}_std_bandpower'] = np.nanstd(bandpower_per_ch) # variability across channels
+
+            # lateralized power differences
+            if bandpower_per_ch.shape[0] == 2:
+                abs_power_features[f'{band}_lr_diff'] = bandpower_per_ch[0] - bandpower_per_ch[1]
+            else:
+                abs_power_features[f'{band}_lr_diff'] = 0.0
 
         # relative bandpower
-        total_power = sum(abs_power_features.values()) 
+        total_power = sum(abs_power_features[f"{band}_abs_bandpower"] for band in bands)        
+        
         for band in bands:
             key = f'{band}_abs_bandpower'
             rel_power_features[f'{band}_rel_bandpower'] = abs_power_features[key] / (total_power + eps) 
@@ -132,8 +222,11 @@ def feature_extraction(epochs):
         ratio_features["theta_alpha_ratio"] = abs_power_features["theta_abs_bandpower"] / (abs_power_features["alpha_abs_bandpower"] + eps)
         ratio_features["delta_beta_ratio"]  = abs_power_features["delta_abs_bandpower"] / (abs_power_features["beta_abs_bandpower"] + eps)
         ratio_features["alpha_beta_ratio"]  = abs_power_features["alpha_abs_bandpower"] / (abs_power_features["beta_abs_bandpower"] + eps)
+        ratio_features["theta_beta_ratio"] = abs_power_features["theta_abs_bandpower"] / (abs_power_features["beta_abs_bandpower"] + eps)
+        ratio_features["alpha_delta_ratio"] = abs_power_features["alpha_abs_bandpower"] / (abs_power_features["delta_abs_bandpower"] + eps)
+        ratio_features["theta_delta_ratio"] = abs_power_features["theta_abs_bandpower"] / (abs_power_features["delta_abs_bandpower"] + eps)
 
-        # Hjord parameters
+        # Hjorth parameters
         activity_per_ch = np.var(valid_epoch, axis=1) # variance of the epoch signal
 
         first_deriv = np.diff(valid_epoch, axis=1)
@@ -152,15 +245,71 @@ def feature_extraction(epochs):
         # spectral entropy
         psd_sum = np.sum(Pxx, axis=1, keepdims=True)
         psd_norm = Pxx / (psd_sum + eps)
+
         entropy_per_ch = -np.sum(psd_norm * np.log2(psd_norm + eps), axis=1)
+        entropy_per_ch /= np.log2(psd_norm.shape[1])        
         spectral_entropy_features["spectral_entropy"] = np.mean(entropy_per_ch)
+
+        # 1/f features
+        fm = SpectralModel(
+            peak_width_limits=[1, 12],
+            max_n_peaks=6,
+            min_peak_height=0.1,
+            verbose=False
+        )
+        
+        aperiodic_exponents = []
+        aperiodic_offsets = []
+        alpha_peak_powers = []
+        alpha_peak_freqs = []
+
+        for ch in range(Pxx.shape[0]):
+
+            fm.fit(f, Pxx[ch])
+
+            # aperiodic
+            offset, exponent = fm.aperiodic_params_
+            aperiodic_offsets.append(offset)
+            aperiodic_exponents.append(exponent)
+
+            # peaks
+            peaks = fm.peak_params_
+            
+            # extract alpha peak (8–13 Hz)
+            alpha_peak = [p for p in peaks if 8 <= p[0] <= 13]
+
+            if len(alpha_peak) > 0:
+                best_peak = max(alpha_peak, key=lambda x: x[1])  # highest power
+                alpha_peak_powers.append(best_peak[1])
+                alpha_peak_freqs.append(best_peak[0])
+
+            else:
+                alpha_peak_powers.append(np.nan)
+                alpha_peak_freqs.append(np.nan)
+
+        if len(aperiodic_exponents) == 2:
+            lr_diff = aperiodic_exponents[0] - aperiodic_exponents[1]
+        else:
+            lr_diff = 0.0
+
+        specparam_features = {
+            "aperiodic_exponent_mean": np.mean(aperiodic_exponents), 
+            "aperiodic_exponent_std": np.std(aperiodic_exponents), 
+            "aperiodic_offset_mean": np.mean(aperiodic_offsets), 
+            "aperiodic_offset_std": np.std(aperiodic_offsets), 
+            "alpha_peak_power_mean": np.nanmean(alpha_peak_powers),
+            "alpha_peak_freq_mean": np.nanmean(alpha_peak_freqs), 
+            "aperiodic_exponent_lr_diff": lr_diff
+        }
 
         epoch_features = (
             abs_power_features
             | rel_power_features
             | ratio_features
+            | peak_features
             | hjorth_features
             | spectral_entropy_features
+            | specparam_features
         )
 
         features.append(epoch_features)
@@ -178,8 +327,16 @@ def feature_extraction(epochs):
         "delta_alpha_ratio",
         "theta_alpha_ratio",
         "delta_beta_ratio",
-        "alpha_beta_ratio",
-    ]
+        "alpha_beta_ratio", 
+        "theta_beta_ratio", 
+        "alpha_delta_ratio", 
+        "theta_delta_ratio", 
+        "delta_std_bandpower",
+        "theta_std_bandpower",
+        "alpha_std_bandpower",
+        "beta_std_bandpower",
+        "gamma_std_bandpower"
+        ]
 
     # log transform for commonly skewed features
     features_df[log_cols] = np.log10(features_df[log_cols] + 1e-12)
